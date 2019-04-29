74f6e730ac7db69adf9a2cd9cc3fafde916a62cf
newpanjing <newpanjing@163.com>
2019-04-29 16:38:04 +0800

    增加导入导出插件的支持

--- finance/admin.py
+++ finance/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from finance.models import *
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class ProxyResource(resources.ModelResource):
+    class Meta:
+        model = Record
+
 
 # Register your models here.
 @admin.register(Record)
-class RecordAdmin(admin.ModelAdmin):
+# class RecordAdmin(admin.ModelAdmin):
+class RecordAdmin(ImportExportModelAdmin):
+
+    resource_class = ProxyResource
+
     list_display = ('id', 'name', 'type', 'money', 'create_date')
     list_per_page = 10
--- simpleui_demo/settings.py
+++ simpleui_demo/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*', ]
 # Application definition
 
 INSTALLED_APPS = [
+    'import_export',
     'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
